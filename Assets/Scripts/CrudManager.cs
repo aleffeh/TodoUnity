aa5cf777dfe0aca5256eb2b460817e52a6eb2744
Felipe Oliveira <felipexk1@live.com>
2020-02-23 14:46:27 -0300

    Dialog and Animations

---
+++ Assets/Scripts/CrudManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CrudManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _canvas;
+    [SerializeField] private GameObject _prefabDialog;
+    [SerializeField] private Button _addButton;
+    
+    
+    private DialogController _dialogController;
+    
+    void Start()
+    {
+        _dialogController = DialogController.NewInputDialog(_canvas, _prefabDialog,"dksds", (dialog) => { Debug.Log(dialog.Text); }, (() => { }));
+        _addButton.onClick.AddListener(() => _dialogController.Show());
+    }
+    
+    
+}
---
+++ Assets/Scripts/DialogStateMachine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class DialogStateMachine : MonoBehaviour
+{
+    private Animator _animator;
+    private static readonly int IsHidden = Animator.StringToHash("isHidden");
+
+    public void ChangeState(bool hide)
+    {
+        gameObject.SetActive(true);
+        if (_animator != null)
+            _animator.SetBool(IsHidden, hide);
+        else
+        {
+            _animator = GetComponent<Animator>();
+            ChangeState(hide);
+        }
+    }
+}
